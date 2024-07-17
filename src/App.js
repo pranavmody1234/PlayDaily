b95990c55879868ce8abc310544d3ab8b53cfb20
pranavmody1234 <pranavmody1234@gmail.com>
2024-07-17 00:40:53 -0500

    HashRouter

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ function App() {
         <Routes>
           <Route path='/' element={
             <>
+            
             <CurrentTheme />
               <DateCarousel />
               <GamesSection />
